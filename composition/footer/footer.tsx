a91e9d9a7fc7a3a8089213baed4c7d61ea050a03
h_tkkk <ittaek0310@naver.com>
2024-05-10 10:52:11 +0900

    footer 변경 및 포트폴리오 레이아웃 생성

--- composition/footer/footer.tsx
+++ composition/footer/footer.tsx
@@ -1,13 +1,25 @@
 import { Box, Typography } from "@mui/material";
+import MyIcon from "@assets/sabuzak_icon_white.svg";
 
 export default function Footer() {
     return (
-        <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
-            <Typography fontWeight={700}>서울특별시 강남구 학동로7길 29</Typography>
-            <Box display={"flex"}>
-                <Typography>sabuzakx2studio@gmail.com</Typography>
-                <Typography>{" | "}</Typography>
-                <Typography>02-6213-3100</Typography>
+        <Box display={"flex"} justifyContent={"space-between"} alignItems={"center"} bgcolor={"#000"} height={120} px={"10%"}>
+            <Box>
+                <MyIcon fill={"#fff"} />
+            </Box>
+            <Box display={"flex"} gap={3}>
+                <Typography fontWeight={500} fontSize={18} color={"#fff"}>
+                    {"Copyright 2024. Sabuzak Studio. All rights reserved."}
+                </Typography>
+                <Typography fontWeight={500} fontSize={18} color={"#fff"}>
+                    {"서울특별시 강남구 학동로7길 29"}
+                </Typography>
+                <Typography fontWeight={500} fontSize={18} color={"#fff"}>
+                    {"02-6213-3100"}
+                </Typography>
+                <Typography fontWeight={500} fontSize={18} color={"#fff"}>
+                    {"sabuzakx2studio@gmail.com"}
+                </Typography>
             </Box>
         </Box>
     );
