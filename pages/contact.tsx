8ca5fd69824ba30c1cd0ac80da3fba14e401659d
h_tkkk <ittaek0310@naver.com>
2024-05-14 14:05:05 +0900

    1. kakaomap 수정 2. 스크롤시 에로우함수 색변경

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Typography } from "@mui/material";
 import Header from "../composition/header/header";
 import Footer from "../composition/footer/footer";
-import { Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
+import { CustomOverlayMap, Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
 
 export default function ContactPage() {
     const [loading, error] = useKakaoLoader({
@@ -21,53 +21,55 @@ export default function ContactPage() {
             <Header />
 
             <Box display={"flex"} flexDirection={"column"} justifyContent={"space-between"} width={"100%"} height={"100%"}>
-                {loading ? (
-                    <Box>
-                        <Typography>Kakao Map을 불러오는 중</Typography>
-                    </Box>
-                ) : (
-                    <Box display={"flex"} flexDirection={"column"} pt={8} height={"100%"}>
-                        <Map center={{ lat: 37.5138, lng: 127.023012 }} style={{ width: "100%", height: "466px" }}>
-                            <MapMarker position={{ lat: 37.5138, lng: 127.023012 }}>
-                                <Typography fontWeight={700}>SABUZAK STUDIO</Typography>
-                            </MapMarker>
-                        </Map>
-
-                        <Box display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
-                            <Typography fontFamily={"Pretendard"} fontWeight={900} fontSize={60}>
-                                Contact
+                <Box display={"flex"} flexDirection={"column"} pt={8} height={"100%"}>
+                    <Map
+                        center={{ lat: 37.5138, lng: 127.023012 }}
+                        style={{ width: "100%", height: "466px" }}
+                        level={2}
+                        draggable={false}
+                        zoomable={false}
+                    >
+                        <CustomOverlayMap position={{ lat: 37.5138, lng: 127.023012 }}>
+                            <Typography fontFamily={"Pretendard"} fontWeight={700}>
+                                SABUZAK STUDIO
                             </Typography>
-                            <Box display={"flex"} flexDirection={"column"} alignItems={"center"} gap={3}>
-                                <Typography fontFamily={"Pretendard"} fontWeight={600} sx={{ fontSize: { xs: "10px", md: "14px", lg: "28px" } }}>
-                                    {"서울특별시 강남구 학동로7길 29"}
-                                </Typography>
-                                <Typography fontFamily={"Pretendard"} fontWeight={600} sx={{ fontSize: { xs: "10px", md: "14px", lg: "28px" } }}>
-                                    {"(02)-6213-3100 (평일 09:00 ~ 18:00)"}
-                                </Typography>
-                                <Typography fontFamily={"Pretendard"} fontWeight={600} sx={{ fontSize: { xs: "10px", md: "14px", lg: "28px" } }}>
-                                    {"sabuzakx2studio@gmail.com"}
-                                </Typography>
-                            </Box>
-                            <Box>
-                                <Button
-                                    onClick={handleEmailSend}
-                                    sx={{
-                                        px: 2,
+                        </CustomOverlayMap>
+                    </Map>
+
+                    <Box display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
+                        <Typography fontFamily={"Pretendard"} fontWeight={900} fontSize={60}>
+                            Contact
+                        </Typography>
+                        <Box display={"flex"} flexDirection={"column"} alignItems={"center"} gap={3}>
+                            <Typography fontFamily={"Pretendard"} fontWeight={600} sx={{ fontSize: { xs: "10px", md: "14px", lg: "28px" } }}>
+                                {"서울특별시 강남구 학동로7길 29"}
+                            </Typography>
+                            <Typography fontFamily={"Pretendard"} fontWeight={600} sx={{ fontSize: { xs: "10px", md: "14px", lg: "28px" } }}>
+                                {"(02)-6213-3100 (평일 09:00 ~ 18:00)"}
+                            </Typography>
+                            <Typography fontFamily={"Pretendard"} fontWeight={600} sx={{ fontSize: { xs: "10px", md: "14px", lg: "28px" } }}>
+                                {"sabuzakx2studio@gmail.com"}
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <Button
+                                onClick={handleEmailSend}
+                                sx={{
+                                    px: 2,
+                                    background: "#000",
+                                    borderRadius: 4,
+                                    "&:hover": {
                                         background: "#000",
-                                        borderRadius: 4,
-                                        "&:hover": {
-                                            background: "#000",
-                                        },
-                                    }}
-                                >
-                                    <Typography fontSize={16} fontWeight={500} color={"#fff"}>
-                                        {"제휴문의 보내기"}
-                                    </Typography>
-                                </Button>
-                            </Box>
+                                    },
+                                }}
+                            >
+                                <Typography fontSize={16} fontWeight={500} color={"#fff"}>
+                                    {"제휴문의 보내기"}
+                                </Typography>
+                            </Button>
                         </Box>
                     </Box>
-                )}
+                </Box>
                 <Footer />
             </Box>
         </Box>
