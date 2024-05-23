83694b0ced8e9ec3b3ce522c693342da33517576
h_tkkk <ittaek0310@naver.com>
2024-05-23 10:00:54 +0900

    naver seo file

--- pages/login/[slag].tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-    return (
-        <div>
-            <h3>슬러그~</h3>
-        </div>
-    );
-}
--- pages/test.tsx
+++
@@ -1,7 +0,0 @@
-export default function TestPage() {
-    return (
-        <div>
-            <h2>22</h2>
-        </div>
-    );
-}
