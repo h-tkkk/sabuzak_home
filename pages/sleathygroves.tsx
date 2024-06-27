629a2c2bc513b7769801d5cab489ba4644cb5e0a
h_tkkk <ittaek0310@naver.com>
2024-06-27 16:46:31 +0900

    test seo

--- pages/sleathygroves.tsx
+++ pages/sleathygroves.tsx
@@ -35,6 +35,7 @@ export default function SleathygrovesPage() {
                 description={
                     "우리 근처에 있지만, 우리는 눈치채지 못하는 작고 꾀죄죄한 곳 털복숭이 숲요정 '그로비' 들과 해발고도 4cm,세상에서 가장 작은 산 '마운틴 옹' 그리고 조금은 특별한 색채를 가진 숲속 친구들의 이야기"
                 }
+                canonical={url}
                 openGraph={{
                     title: title,
                     description: description,
