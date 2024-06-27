38f8d944241d21975c29e83132dcdfb0576c589b
h_tkkk <ittaek0310@naver.com>
2024-06-27 16:21:45 +0900

    tset

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,46 +45,44 @@ export default function Home() {
     };
 
     return (
-        <>
-            <Head>
-                <title>Sabuzak Studio</title>
-                <meta name="description" content="사부작사부작 스튜디오" />
-                <meta
-                    name="keywords"
-                    content="sbz,사부작 스튜디오,사부작,햄밍웨이,콤플렉스 아카데미,쑝쑝 메거진,꾀죄죄한작은숲,꾀작숲,사부작사부작스튜디오"
-                />
-                {/* Open Graph meta tags */}
-                <meta property="og:title" content="Sabuzakx2 Studio" />
-                <meta property="og:description" content="Sabuzakx2 Studio" />
-                <meta property="og:image" content="https://www.sbzsbz.com/img/main_section/full_img.png" />
-                <link rel="canonical" href="https://www.sbzsbz.com" />
-            </Head>
-            <Box height={"100%"} display={"flex"} flexDirection={"column"} justifyContent={"space-between"} position={"relative"}>
-                <Header />
-                {/* Content */}
-                <Box width={"100%"} height={"100%"}>
-                    <HomeSection />
-                    <AboutSection />
-                    {isMobile ? <CenterSwiper /> : <PortfolioSection />}
-                    <ContextUs />
-                    <Footer />
-                </Box>
-                {/* Array Navigation */}
-                <ArrowNavigation />
-                <Zoom
-                    in={showFloatingButton}
-                    timeout={transitionDuration}
-                    style={{
-                        transitionDelay: `${showFloatingButton ? transitionDuration.exit : 0}ms`,
-                        backgroundColor: "#133F7F",
-                    }}
-                    unmountOnExit
-                >
-                    <Fab color="primary" aria-label="add" style={{ position: "fixed", right: 20, bottom: 20 }} onClick={handleHomeSectionMove}>
-                        <ArrowUpwardIcon />
-                    </Fab>
-                </Zoom>
+        // <Head>
+        //     <title>Sabuzak Studio</title>
+        //     <meta name="description" content="사부작사부작 스튜디오" />
+        //     <meta
+        //         name="keywords"
+        //         content="sbz,사부작 스튜디오,사부작,햄밍웨이,콤플렉스 아카데미,쑝쑝 메거진,꾀죄죄한작은숲,꾀작숲,사부작사부작스튜디오"
+        //     />
+        //     {/* Open Graph meta tags */}
+        //     <meta property="og:title" content="Sabuzakx2 Studio" />
+        //     <meta property="og:description" content="Sabuzakx2 Studio" />
+        //     <meta property="og:image" content="https://www.sbzsbz.com/img/main_section/full_img.png" />
+        //     <link rel="canonical" href="https://www.sbzsbz.com" />
+        // </Head>
+        <Box height={"100%"} display={"flex"} flexDirection={"column"} justifyContent={"space-between"} position={"relative"}>
+            <Header />
+            {/* Content */}
+            <Box width={"100%"} height={"100%"}>
+                <HomeSection />
+                <AboutSection />
+                {isMobile ? <CenterSwiper /> : <PortfolioSection />}
+                <ContextUs />
+                <Footer />
             </Box>
-        </>
+            {/* Array Navigation */}
+            <ArrowNavigation />
+            <Zoom
+                in={showFloatingButton}
+                timeout={transitionDuration}
+                style={{
+                    transitionDelay: `${showFloatingButton ? transitionDuration.exit : 0}ms`,
+                    backgroundColor: "#133F7F",
+                }}
+                unmountOnExit
+            >
+                <Fab color="primary" aria-label="add" style={{ position: "fixed", right: 20, bottom: 20 }} onClick={handleHomeSectionMove}>
+                    <ArrowUpwardIcon />
+                </Fab>
+            </Zoom>
+        </Box>
     );
 }
