c2bd4c67c3a06ffd8b8a74da8932481c9d86a548
h_tkkk <ittaek0310@naver.com>
2024-06-28 09:20:38 +0900

    SSR TEST

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,58 +1,10 @@
-import { Box, Grid, Paper, Typography } from "@mui/material";
-import MainLayout from "../composition/layout/main_layout";
+import { Box } from "@mui/material";
 import Header from "../composition/header/header";
-import Footer from "../composition/footer/footer";
-import PfEx1 from "../module/portfolio_page/ex1";
 import ImgEx from "../module/portfolio_page/img_ex";
 import Head from "next/head";
+import { GetServerSideProps } from "next";
 
-interface PortfolioData {
-    title: string;
-    englishTitle: string;
-    mainContent: string;
-    subContent: string;
-    imgList: string[];
-}
-
-const data: PortfolioData[] = [
-    {
-        title: "콤플렉스 아카데미",
-        englishTitle: "Complex Academy",
-        mainContent: "콤플렉스를 극복하지 않아도\n있는 그대로의 내가 행복이다.",
-        subContent: "라미와 친구들의 마법 학교 일상을 통해 보여주는\n여러가지 콤플렉스와 그를 통해 성장하는 이야기",
-        imgList: ["/img/margazine_1.png", "/img/margazine_2.png", "/img/margazine_3.png"],
-    },
-    {
-        title: "햄밍웨이",
-        englishTitle: "Hamingway",
-        mainContent: "나, 햄밍웨이 1.3세, 70구람!🐹\n눈물 한 방울 💧 흘려 본 적 없는 싸나이!",
-        subContent: "멋진 선장이 되고픈 햄밍웨이와\n햄스터 마을 친구들의 아기자기한 일상",
-        imgList: ["/img/hamingway_1.png", "/img/hamingway_2.png", "/img/hamingway_3.png"],
-    },
-    {
-        title: "쑝쑝 매거진",
-        englishTitle: "ssyong.ssyong margazine",
-        mainContent: "얼렁뚱땅 짧툰",
-        subContent: "리액션 맛집 동물들이 보여주는\n일상 속 새로운 시선과 재미있는 리액셕의 짧툰",
-        imgList: ["/img/margazine_1.png", "/img/margazine_2.png", "/img/margazine_3.png"],
-    },
-    {
-        title: "콤플렉스 아카데미",
-        englishTitle: "Complex Academy",
-        mainContent: "콤플렉스를 극복하지 않아도\n있는 그대로의 내가 행복이다.",
-        subContent: "라미와 친구들의 마법 학교 일상을 통해 보여주는\n여러가지 콤플렉스와 그를 통해 성장하는 이야기",
-        imgList: ["/img/margazine_1.png", "/img/margazine_2.png", "/img/margazine_3.png"],
-    },
-    {
-        title: "햄밍웨이",
-        englishTitle: "Hamingway",
-        mainContent: "나, 햄밍웨이 1.3세, 70구람!🐹\n눈물 한 방울 💧 흘려 본 적 없는 싸나이!",
-        subContent: "멋진 선장이 되고픈 햄밍웨이와\n햄스터 마을 친구들의 아기자기한 일상",
-        imgList: ["/img/hamingway_1.png", "/img/hamingway_2.png", "/img/hamingway_3.png"],
-    },
-];
-
-export default function portfolioPage() {
+const PortfolioPage = () => {
     return (
         <>
             <Head>
@@ -69,4 +21,12 @@ export default function portfolioPage() {
             </Box>
         </>
     );
-}
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    return {
+        props: {}, // 나중에 필요한 데이터를 여기서 전달할 수 있습니다.
+    };
+};
+
+export default PortfolioPage;
--- pages/sleathygroves.tsx
+++ pages/sleathygroves.tsx
@@ -1,8 +1,6 @@
-import { Box, Typography } from "@mui/material";
-import Head from "next/head";
+import { Box } from "@mui/material";
 import Header from "../composition/header/header";
 import MainLayout from "../module/sleathygroves_page/main_layout";
-import PortfolioHead from "../component/portfolio_head";
 import { NextSeo } from "next-seo";
 import { GetServerSideProps } from "next";
 
@@ -16,30 +14,8 @@ interface HomePageProps {
 }
 
 export default function SleathygrovesPage({ seo }: HomePageProps) {
-    const title = "꾀죄죄한 작은 숲";
-    const description =
-        "우리 근처에 있지만, 우리는 눈치채지 못하는 작고 꾀죄죄한 곳 털복숭이 숲요정 '그로비' 들과 해발고도 4cm,세상에서 가장 작은 산 '마운틴 옹' 그리고 조금은 특별한 색채를 가진 숲속 친구들의 이야기";
-    const images = "https://www.sbzsbz.com/img/sleathygroves_banner.png";
-    const url = "https://www.sbzsbz.com/sleathygroves";
-
     return (
         <>
-            {/* <PortfolioHead
-                title="꾀죄죄한 작은 숲"
-                description="우리 근처에 있지만, 우리는 눈치채지 못하는 작고 꾀죄죄한 곳 털복숭이 숲요정 '그로비' 들과 해발고도 4cm,세상에서 가장 작은 산 '마운틴 옹' 그리고 조금은 특별한 색채를 가진 숲속 친구들의 이야기"
-                ogImage="sleathygroves_banner.png"
-                keywords="꾀죄죄,꾀죄죄한 작은 숲,마운팅옹,그로비,씨조새,숲요정,해발고도"
-                subUrl="sleathygroves"
-            /> */}
-            {/* <Head>
-                <title>꾀죄죄한 작은 숲 - Sabuzak Studio</title>
-                <meta name="description" content="꾀죄죄한 작은 숲 - Sabuzak Studio." />
-                <meta property="og:title" content="꾀죄죄한 작은 숲 - Sabuzak Studio" />
-                <meta property="og:description" content="꾀죄죄한 작은 숲 - Sabuzak Studio" />
-                <meta property="og:image" content="https://www.sbzsbz.com/img/sleathygroves_banner.png" />
-                <link rel="canonical" href="https://www.sbzsbz.com/sleathygroves" />
-                <meta name="keywords" content="꾀죄죄,꾀죄죄한 작은 숲,마운팅옹,그로비,씨조새,숲요정,해발고도" />
-            </Head> */}
             <NextSeo
                 title={seo.title}
                 description={seo.description}
@@ -68,12 +44,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
     const seo = {
         title: "꾀죄죄한 작은 숲 - Sabuzakx2 Studio",
         description:
-            "우리 근처에 있지만, 우리는 눈치채지 못하는 작고 꾀죄죄한 곳 털복숭이 숲요정 '그로비' 들과 해발고도 4cm,세상에서 가장 작은 산 '마운틴 옹' 그리고 조금은 특별한 색채를 가진 숲속 친구들의 이야기",
+            "우리 근처에 있지만, 우리는 눈치채지 못하는 작고 꾀죄죄한 곳 털복숭이 숲요정 '그로비' 들과 해발고도 4cm, 세상에서 가장 작은 산 '마운틴 옹' 그리고 조금은 특별한 색채를 가진 숲속 친구들의 이야기",
         url: "https://www.sbzsbz.com/sleathygroves",
         image: "https://www.sbzsbz.com/img/sleathygroves_banner.png",
     };
-
-    console.log(seo);
 
     return {
         props: {
