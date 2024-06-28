71a30274a4d8ce2b2e8db3875f23eae559e6094f
h_tkkk <ittaek0310@naver.com>
2024-06-28 14:08:26 +0900

    build error arrow navigation

--- component/arrow_navigation.tsx
+++ component/arrow_navigation.tsx
@@ -11,25 +11,29 @@ export default function ArrowNavigation() {
         let targetSectionIndex = 0;
         for (let i = 0; i < sections.length; i++) {
             const element = document.getElementById(sections[i]);
-            const rect = element.getBoundingClientRect();
+            if (element) {
+                const rect = element.getBoundingClientRect();
 
-            // 현재 화면에서 부분적으로 보이는 섹션을 찾는 로직
-            if (rect.bottom > 0 && rect.top < window.innerHeight) {
-                if (rect.bottom > window.innerHeight) {
-                    // 섹션의 하단이 화면 밖에 있으면 그 섹션으로 스크롤
-                    targetSectionIndex = i;
-                } else {
-                    // 섹션이 완전히 화면에 보이면 다음 섹션으로 이동
-                    targetSectionIndex = (i + 1) % sections.length;
+                // 현재 화면에서 부분적으로 보이는 섹션을 찾는 로직
+                if (rect.bottom > 0 && rect.top < window.innerHeight) {
+                    if (rect.bottom > window.innerHeight) {
+                        // 섹션의 하단이 화면 밖에 있으면 그 섹션으로 스크롤
+                        targetSectionIndex = i;
+                    } else {
+                        // 섹션이 완전히 화면에 보이면 다음 섹션으로 이동
+                        targetSectionIndex = (i + 1) % sections.length;
+                    }
+                    break;
                 }
-                break;
             }
         }
 
         const nextSection = sections[targetSectionIndex];
-        document.getElementById(nextSection).scrollIntoView({ behavior: "smooth" });
+        const nextElement = document.getElementById(nextSection);
+        if (nextElement) {
+            nextElement.scrollIntoView({ behavior: "smooth" });
+        }
     };
-
     useEffect(() => {
         const checkScroll = () => {
             const homeElement = document.getElementById("home");
