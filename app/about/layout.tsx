86ae9a6457ff4f66ed6170bde69f9bac6915fd06
h_tkkk <ittaek0310@naver.com>
2024-06-28 15:26:23 +0900

    about og:image modiy

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -13,7 +13,7 @@ export async function generateMetadata() {
             description: "사부작사부작 스튜디오의 소개 페이지입니다. 창의적이고 멋진 아이디어와 함께합니다.",
             images: [
                 {
-                    url: "https://www.sbzsbz.com/img/about_banner.png",
+                    url: "https://www.sbzsbz.com/img/og_about.png",
                     width: 1200,
                     height: 630,
                     alt: "About Sabuzakx2 Studio",
